a7de85054e0a2921336c6f925aefc3ad861f4531
Timo Bechtel <hi@timobechtel.com>
2021-04-18 22:54:49 +0200

    chore(deps): bump
    .
    
    closes #6

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,1 @@
-declare module 'assign';
-/**
- * Assigns object/array to object/array
- * @param target Object or array to assign to
- * @param source Source object or array
- */
-export function assign(target: object | any[], source: object | any[]): void;
+export function assign(target: any, source: any): void;
